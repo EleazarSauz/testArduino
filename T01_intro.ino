6114044c818a3dea8bc6925574e62b698674d301
EleazarSauz <eleazar48a@gmail.com>
2018-07-16 00:02:07 -0500

    firstCommit

---
+++ T01_intro.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(12, OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(12, HIGH);
+delay(0.1);
+digitalWrite(12, LOW);
+delay(0.1);
+digitalWrite(12, HIGH);
+delay(0.1);
+digitalWrite(12, LOW);
+delay(0.1);
+digitalWrite(12, HIGH);
+delay(0.1);
+digitalWrite(12, LOW);
+delay(0.1);
+digitalWrite(12, HIGH);
+delay(0.1);
+digitalWrite(12, LOW);
+delay(0.1);
+digitalWrite(12, HIGH);
+delay(0.1);
+digitalWrite(12, LOW);
+delay(0.1);
+}
